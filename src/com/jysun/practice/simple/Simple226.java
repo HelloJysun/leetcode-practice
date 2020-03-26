425406e56307187bbed989ca8dd80c6dd4c93f45
Today1sFr1day <hellojysun@qq.com>
2020-03-26 20:23:24 +0800

    翻转二叉树

---
+++ src/com/jysun/practice/simple/Simple226.java
@@ -0,0 +1,71 @@
+package com.jysun.practice.simple;
+
+import com.jysun.practice.datastruct.TreeNode;
+
+/**
+ * @author Jysun
+ * @description  翻转二叉树
+ *
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ * @date 2020/3/25 10:11
+ */
+public class Simple226 {
+
+    public static void main(String[] args) {
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node9 = new TreeNode(9);
+        node4.left = node2;
+        node4.right = node7;
+        node2.left = node1;
+        node2.right = node3;
+        node7.left = node6;
+        node7.right = node9;
+        Simple226 obj = new Simple226();
+        TreeNode res = obj.invertTree(node4);
+        System.out.println(res);
+    }
+
+    /**
+     * 递归解法
+     * 递推公式：左右子树交换。左子树、右子树也递归交换
+     * 终止条件：null节点直接返回
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
