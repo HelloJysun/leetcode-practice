c4f27cea8847441ef343b072e6d39fe6052eca7b
Today1sFr1day <hellojysun@qq.com>
2020-04-27 23:11:11 +0800

    搜索旋转排序数组

---
+++ src/com/jysun/practice/medium/Medium33.java
@@ -0,0 +1,59 @@
+package com.jysun.practice.medium;
+
+/**
+ * @author Jysun
+ * @description Medium33 搜索旋转排序数组
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ *
+ * 你可以假设数组中不存在重复的元素。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * 示例 2:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ *
+ * @date 2020/4/27 21:54
+ */
+public class Medium33 {
+
+    public static void main(String[] args) {
+        Medium33 obj = new Medium33();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(obj.search(nums, 0));
+        System.out.println(obj.search(nums, 3));
+    }
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) { // 左侧有序，考虑两个元素情况。nums[left] <= nums[mid]
+                if (target < nums[mid] && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // 右侧有序
+                if (target > nums[mid] && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
