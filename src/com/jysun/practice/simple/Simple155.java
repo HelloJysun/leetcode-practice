869e2a785129642aaaa6fe4a29215a7240b6466d
Today1sFr1day <hellojysun@qq.com>
2020-05-12 21:44:53 +0800

    最小栈

---
+++ src/com/jysun/practice/simple/Simple155.java
@@ -0,0 +1,80 @@
+package com.jysun.practice.simple;
+
+import java.util.Stack;
+
+/**
+ * @author Jysun
+ * @description Simple155 最小栈
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) —— 将元素 x 推入栈中。
+ * pop() —— 删除栈顶的元素。
+ * top() —— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入：
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * <p>
+ * 输出：
+ * [null,null,null,null,-3,null,0,-2]
+ * <p>
+ * 解释：
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * @date 2020/5/12 21:22
+ */
+public class Simple155 {
+
+    public static void main(String[] args) {
+        MinStack minStack = new Simple155().new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        minStack.getMin();
+        minStack.pop();
+        minStack.top();
+        minStack.getMin();
+    }
+
+    class MinStack {
+
+        Stack<Integer> stack;
+        Stack<Integer> minStack;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MinStack() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            minStack.push(!minStack.isEmpty() && minStack.peek() < x ? minStack.peek() : x);
+        }
+
+        public void pop() {
+            stack.pop();
+            minStack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+}
