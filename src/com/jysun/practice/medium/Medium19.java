36781b63878d8e540c7884cb46463e185581ac36
Todaye1sFr1day <hellojysun@qq.com>
2020-08-21 17:58:50 +0800

    删除链表的倒数第N个节点

---
+++ src/com/jysun/practice/medium/Medium19.java
@@ -0,0 +1,67 @@
+package com.jysun.practice.medium;
+
+import com.jysun.practice.datastruct.ListNode;
+
+/**
+ * @author Jysun
+ * @description 删除链表的倒数第N个节点
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ * @date 2020/8/21 17:12
+ */
+public class Medium19 {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        Medium19 obj = new Medium19();
+        System.out.println(obj.removeNthFromEnd(node1, 2));
+
+        ListNode node = new ListNode(1);
+        System.out.println(obj.removeNthFromEnd(node, 1));
+    }
+
+    /**
+     * tip：一开始没有使用dummy哑结点，会导致单个结点删除时需要额外判断以防NPE，添加哑结点可以统一操作
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        while (n-- != 0) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
