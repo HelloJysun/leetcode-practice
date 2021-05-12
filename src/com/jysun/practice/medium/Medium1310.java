51a8adf27c483105a3bc29fa58434e1a756557b2
Today1sFr1day <hellojysun@qq.com>
2021-05-12 22:54:22 +0800

    子数组异或查询

---
+++ src/com/jysun/practice/medium/Medium1310.java
@@ -0,0 +1,82 @@
+package com.jysun.practice.medium;
+
+import java.util.Arrays;
+
+/**
+ * @author Jysun
+ * @description 子数组异或查询
+ * 有一个正整数数组 arr，现给你一个对应的查询数组 queries，其中 queries[i] = [Li, Ri]。
+ * <p>
+ * 对于每个查询 i，请你计算从 Li 到 Ri 的 XOR 值（即 arr[Li] xor arr[Li+1] xor ... xor arr[Ri]）作为本次查询的结果。
+ * <p>
+ * 并返回一个包含给定查询 queries 所有结果的数组。
+ * <p>
+ * 示例 1：
+ * 输入：arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+ * 输出：[2,7,14,8]
+ * 解释：
+ * 数组中元素的二进制表示形式是：
+ * 1 = 0001
+ * 3 = 0011
+ * 4 = 0100
+ * 8 = 1000
+ * <p>
+ * 查询的 XOR 值为：
+ * [0,1] = 1 xor 3 = 2
+ * [1,2] = 3 xor 4 = 7
+ * [0,3] = 1 xor 3 xor 4 xor 8 = 14
+ * [3,3] = 8
+ * <p>
+ * 示例 2：
+ * 输入：arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+ * 输出：[8,0,4,4]
+ * <p>
+ * <p>
+ * 提示：
+ * 1 <= arr.length <= 3 * 10^4
+ * 1 <= arr[i] <= 10^9
+ * 1 <= queries.length <= 3 * 10^4
+ * queries[i].length == 2
+ * 0 <= queries[i][0] <= queries[i][1] < arr.length
+ * @date 2021/5/12 19:37
+ */
+public class Medium1310 {
+
+    public static void main(String[] args) {
+        Medium1310 obj = new Medium1310();
+        int[] arr = {1, 3, 4, 8};
+        int[][] queries = {{0, 1}, {1, 2}, {0, 3}, {3, 3}};
+        int[] res1 = obj.xorQueries(arr, queries);
+        int[] res2 = obj.xorQueriesOpt(arr, queries);
+        System.out.println(Arrays.toString(res1));
+        System.out.println(Arrays.toString(res2));
+    }
+
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] res = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
+            int item = 0;
+            for (int j = query[0]; j <= query[1]; j++) {
+                item = item ^ arr[j];
+            }
+            res[i] = item;
+        }
+
+        return res;
+    }
+
+    public int[] xorQueriesOpt(int[] arr, int[][] queries) {
+        // 计算前缀和
+        int[] xors = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            xors[i + 1] = xors[i] ^ arr[i];
+        }
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            res[i] = xors[queries[i][0]] ^ xors[queries[i][1] + 1];
+        }
+        return res;
+    }
+}
